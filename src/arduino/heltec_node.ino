a7d73bee35698c60d45e4717304dc23407fe96c2
Raffique <raffiquemuir@yahoo.com>
2025-01-16 07:32:46 -0500

    updates

--- src/arduino/heltec_node.ino
+++ src/arduino/heltec_node.ino
@@ -8,8 +8,20 @@ const int BOTTOM_LEVEL_PIN = 5;    // For stations 2 & 3
 const int FAULT_PIN = 4;           // Fault detection
 const int OP_MODE_PIN = 3;         // Operation mode switch
 const int PUMP_CONTROL_PIN = 39;    // Relay to control pump
+const int CONNECTED_STATUS_PIN = 40;    // Relay to connected status
 const int BUTTON1_PIN = 9;         // Info button 1
 const int BUTTON2_PIN = 10;        // Info button 2
+
+// const int PUMP_STATUS_PIN = 7;     // Relay sensing if pump is on/off
+// const int PRESSURE_SWITCH_PIN = 6;  // For station 1 only
+// const int TOP_LEVEL_PIN = 6;       // For stations 2 & 3
+// const int BOTTOM_LEVEL_PIN = 5;    // For stations 2 & 3
+// const int FAULT_PIN = 4;           // Fault detection
+// const int OP_MODE_PIN = 3;         // Operation mode switch
+// const int PUMP_CONTROL_PIN = 8;    // Relay to control pump
+// const int CONNECTED_STATUS_PIN = 9;    // Relay to connected status
+// const int BUTTON1_PIN = 10;         // Info button 1
+// const int BUTTON2_PIN = 11;        // Info button 2
 
 // Timing control
 const unsigned long SEND_INTERVAL = 5000;  // Send status every 5 seconds
@@ -31,6 +43,9 @@ void setup() {
   pinMode(OP_MODE_PIN, INPUT_PULLUP);
   pinMode(BUTTON1_PIN, INPUT_PULLUP);
   pinMode(BUTTON2_PIN, INPUT_PULLUP);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
   
   // Configure output pin
   pinMode(PUMP_CONTROL_PIN, OUTPUT);
@@ -61,7 +76,7 @@ void sendStatus() {
   doc["top_level"] = !digitalRead(TOP_LEVEL_PIN);
   doc["bottom_level"] = !digitalRead(BOTTOM_LEVEL_PIN);
   doc["fault"] = !digitalRead(FAULT_PIN);
-  doc["op_mode"] = !digitalRead(OP_MODE_PIN);
+  if (digitalRead(OP_MODE_PIN) == 1) { doc["op_mode"] = true; } else { doc["op_mode"] = false; };
   
   // Serialize JSON to string
   String jsonString;
@@ -84,5 +99,11 @@ void processCommand(String command) {
     bool pumpState = doc["pump_control"];
     digitalWrite(PUMP_CONTROL_PIN, pumpState);
     
+  }
+
+  if (doc.containsKey("connected_status")){
+    bool connectedStatus = doc["connected_status"];
+    digitalWrite(CONNECTED_STATUS_PIN, connectedStatus);
+    digitalWrite(13, connectedStatus);
   }
 }
